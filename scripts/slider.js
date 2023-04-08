634899bcbf3e7c8dfe522eb7dc582301c751db69
Samuele Mattiuzzo <samumatt@gmail.com>
2023-04-08 15:10:36 +0100

    bla

--- scripts/slider.js
+++ scripts/slider.js
@@ -12,13 +12,11 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
+  let i,
+      slides = document.getElementsByClassName("slide");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  for (i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
   slides[slideIndex-1].style.display = "block";
 }
